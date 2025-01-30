82ca13edc74b401b36c848fd4e65813796dcd57e
tanitaka_tech <78785830+tanitaka-tech@users.noreply.github.com>
2025-01-30 21:54:47 +0900

    Revert "🔧 DIコンテナの実装WIP"
    This reverts commit 11bda24b5735b7f9fe4ff611f097b19392f03d5c.

---
+++ src/NestedDIContainer.Unity/Runtime/ProjectScopeReference.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace NestedDIContainer.Unity.Runtime
+{
+    public class ProjectScopeReference : ScriptableObject
+    {
+        [SerializeField] ProjectScope _projectScope;
+        
+        public ProjectScope ProjectScope
+        {
+            get => _projectScope;
+            set
+            {
+                if (_projectScope != null && value != _projectScope)
+                {
+                    _projectScope = value;
+                }
+            }
+        }
+        
+        internal ProjectScope CreateProjectScope()
+        {
+            if (_projectScope == null)
+                throw new Exception("ProjectScope is not set. Please set the ProjectScope in the ProjectScopeReference.");
+            
+            _projectScope.gameObject.SetActive(false);
+            var instance = Instantiate(_projectScope, null);
+            DontDestroyOnLoad(instance);
+            
+            instance.gameObject.SetActive(true);
+            
+            return instance;
+        }
+    }
+}
---
+++ src/NestedDIContainer.Unity/Runtime/Scopes/MonoBehaviourScope.cs
@@ -0,0 +1,18 @@
+﻿using NestedDIContainer.Unity.Runtime.Core;
+using TanitakaTech.NestedDIContainer;
+
+namespace NestedDIContainer.Unity.Runtime
+{
+    public abstract class MonoBehaviourScope : MonoBehaviourScopeWithConfig<MonoBehaviourScope.EmptyConfig>
+    {
+        public abstract class EmptyConfig { }
+        protected override void Construct(DependencyBinder binder, EmptyConfig config) => Construct(binder);
+        protected abstract void Construct(DependencyBinder binder);
+    }
+    
+    public abstract class MonoBehaviourScopeWithConfig<TConfig> : MonoBehaviourScopeBase
+    {
+        protected override void Construct(DependencyBinder binder, object config) => Construct(binder, config is TConfig c ? c : default);
+        protected abstract void Construct(DependencyBinder binder, TConfig config);
+    }
+}
