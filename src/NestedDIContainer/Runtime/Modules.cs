92c950084799b502e5355a583dbf58f6f62b6caa
tanitaka-tech <78785830+tanitaka-tech@users.noreply.github.com>
2025-04-20 13:24:27 +0900

    perf: Add readonly to local variable

--- src/NestedDIContainer/Runtime/Modules.cs
+++ src/NestedDIContainer/Runtime/Modules.cs
@@ -6,19 +6,19 @@ namespace TanitakaTech.NestedDIContainer
 {
     public class Modules
     {
-        private Dictionary<(ScopeId BelongingScopeId, IntPtr TypePtr), object> Value { get; }
-        private Dictionary<ScopeId, IScope> Scopes { get; }
+        private readonly Dictionary<(ScopeId BelongingScopeId, IntPtr TypePtr), object> _value;
+        private readonly Dictionary<ScopeId, IScope> _scopes;
 
         public Modules(Dictionary<(ScopeId, IntPtr), object> value, Dictionary<ScopeId, IScope> scopes)
         {
-            Value = value;
-            Scopes = scopes;
+            _value = value;
+            _scopes = scopes;
         }
 
         public void Bind(ScopeId belongingScopeId, Type type, object module)
         {
             var key = (belongingScopeId, type.TypeHandle.Value);
-            if (!Value.TryAdd(key, module))
+            if (!_value.TryAdd(key, module))
             {
                 throw new ConstructException($"Module already exists: {type}, {belongingScopeId}");
             }
@@ -27,19 +27,19 @@ namespace TanitakaTech.NestedDIContainer
         public void Remove(ScopeId belongingScopeId, Type type)
         {
             var key = (belongingScopeId, type.TypeHandle.Value);
-            (Value[key] as IDisposable)?.Dispose();
-            Value.Remove(key);
+            (_value[key] as IDisposable)?.Dispose();
+            _value.Remove(key);
         }
 
         public void RemoveScope(ScopeId belongingScopeId)
         {
-            var needRemoveKey = Value.Where(k => k.Key.BelongingScopeId.Equals(belongingScopeId))
+            var needRemoveKey = _value.Where(k => k.Key.BelongingScopeId.Equals(belongingScopeId))
                 .Select(k => k.Key)
                 .ToList();
             foreach (var moduleRelation in needRemoveKey)
             {
-                (Value[moduleRelation] as IDisposable)?.Dispose();
-                Value.Remove(moduleRelation);
+                (_value[moduleRelation] as IDisposable)?.Dispose();
+                _value.Remove(moduleRelation);
             }
             needRemoveKey.Clear();
         }
@@ -51,14 +51,14 @@ namespace TanitakaTech.NestedDIContainer
 
         public T Resolve<T>(ScopeId callScopeId)
         {
-            Scopes.TryGetValue(callScopeId, out var callScope);
+            _scopes.TryGetValue(callScopeId, out var callScope);
             return Resolve<T>(callScope);
         }
 
         public object Resolve(Type type, IScope callScope)
         {
             var typePtr = type.TypeHandle.Value;
-            if (Value.TryGetValue((callScope.ScopeId, typePtr), out var module))
+            if (_value.TryGetValue((callScope.ScopeId, typePtr), out var module))
                 return module;
 
             var parentScopeIdNullable = callScope?.ParentScopeId;
@@ -66,10 +66,10 @@ namespace TanitakaTech.NestedDIContainer
             {
                 var parentScopeId = parentScopeIdNullable.Value;
 
-                if (Value.TryGetValue((parentScopeId, typePtr), out var module2))
+                if (_value.TryGetValue((parentScopeId, typePtr), out var module2))
                     return module2;
 
-                Scopes.TryGetValue(parentScopeId, out var parentScope);
+                _scopes.TryGetValue(parentScopeId, out var parentScope);
                 parentScopeIdNullable = parentScope?.ParentScopeId;
             }
             throw new ConstructException($"Module not found: {type}, {callScope?.ScopeId}");
